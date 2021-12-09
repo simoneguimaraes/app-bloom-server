1375de239730b6141f16531df238a5a51339d453
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-12-08 22:20:15 -0300

    added user schema

---
+++ models/User.model.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  name: { type: String, trim: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  passwordHash: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["DOCTOR", "USER"],
+    required: true,
+    default: "USER",
+  },
+  crmDoctor: {
+    type: String,
+    default: "USER",
+  },
+});
+
+const UserModel = model("User", UserSchema);
+
+module.exports = UserModel;
